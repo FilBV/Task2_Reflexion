a7a57f40e2be83cac38c44d347ef98d4be61821f
FilBV <161864156+FilBV@users.noreply.github.com>
2024-05-26 23:01:45 +0700

    Initial commit

---
+++ src/main/java/ru/inno/tasks/Main.java
@@ -0,0 +1,22 @@
+package ru.inno.tasks;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Fraction fraction = new Fraction(2, 4);
+        Fractionable fraction2 = Utils.cache(fraction);
+
+
+        System.out.println(fraction2.doubleValue());
+        fraction2.setNum(5);
+        System.out.println(fraction2.doubleValue());
+        System.out.println(fraction2.doubleValue());
+        fraction2.setDenum(4);
+        System.out.println(fraction2.doubleValue());
+        System.out.println(fraction2.doubleValue());
+
+
+
+    }
+}
---
+++ src/test/java/CacheHandlerTests.java
@@ -0,0 +1,43 @@
+
+import org.junit.jupiter.api.*;
+import ru.inno.tasks.Fractionable;
+import ru.inno.tasks.Utils;
+
+public class CacheHandlerTests {
+
+    TestFraction fraction = new TestFraction(1, 4);
+    Fractionable fraction2 = Utils.cache(fraction);
+
+    @Test
+    @DisplayName("methods without annotation")
+    public void testNoAnnotationMethods() {
+        fraction2.toString();
+        fraction2.toString();
+        fraction2.toString();
+        Assertions.assertEquals(fraction.count, 3);
+    }
+
+    @Test
+    @DisplayName("methods with annotation dont clear cache")
+    public void testCachNoClear() {
+        fraction2.doubleValue();
+        fraction2.doubleValue();
+        fraction2.toString();
+        fraction2.doubleValue();
+        Assertions.assertEquals(fraction.count, 2);
+    }
+    @Test
+    @DisplayName("methods with annotation clear cache")
+    public void testCachClear() {
+        fraction2.doubleValue();
+        fraction2.setDenum(5);
+        fraction2.toString();
+        fraction2.doubleValue();
+        fraction2.setNum(2);
+        fraction2.doubleValue();
+        fraction2.doubleValue();
+        Assertions.assertEquals(fraction.count, 4);
+    }
+
+
+}
